edafd7b2049e290af1c1b35718f4fe1a5fde575f
intellisense <intellisense@intellisenses-mac-mini.local>
2017-01-17 16:42:56 +0530

    Initial Commit

---
+++ whatsAppMic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  whatsAppMic
+//
+//  Created by intellisense on 17/01/17.
+//  Copyright © 2017 intellisense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ whatsAppMic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  whatsAppMic
+//
+//  Created by intellisense on 17/01/17.
+//  Copyright © 2017 intellisense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
